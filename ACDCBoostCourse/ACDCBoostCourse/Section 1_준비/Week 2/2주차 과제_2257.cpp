277468c9f2af72ac71a75548a56f8fdcf46ef07d
박민준 <blessingpmj@seoultech.ac.kr>
2022-07-12 15:52:41 +0900

    [박민준] 2주차 과제_2577

---
+++ ACDCBoostCourse/ACDCBoostCourse/Section 1_준비/Week 2/2주차 과제_2257.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int num_array[10] = {0};
+	int A, B, C;
+	cin >> A >> B >> C;
+	int num = A * B * C;
+	while (num != 0) {
+		num_array[num % 10]++;
+		num= num / 10;
+	}
+	for (int i = 0; i < 10; i++) {
+		cout << num_array[i] << endl;
+	}
+	return 0;
+}
