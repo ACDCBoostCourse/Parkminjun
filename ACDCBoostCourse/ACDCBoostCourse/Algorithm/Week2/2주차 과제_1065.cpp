013e6006241a78c176db56422ac120098840832d
박민준 <blessingpmj@seoultech.ac.kr>
2022-07-12 21:27:58 +0900

    [박민준]2주차 과제_1065

---
+++ ACDCBoostCourse/ACDCBoostCourse/Algorithm/Week2/2주차 과제_1065.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+
+	cin >> N;
+	if (N < 100) {
+		cout << N;
+	}
+	else {
+		int count = 0;
+		for (int i = 100; i <= N; i++) {
+			int third = i % 10;
+			int second = (i / 10) % 10;
+			int first = i / 100;
+			if ( (first - second) == (second - third) ) {
+				count++;
+			}
+		}
+		cout << 99+count;
+	}
+	
+	return 0;
+}
