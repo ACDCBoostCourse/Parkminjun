acdc509c8cf9b58cd0084818f816a2cb915f4744
박민준 <blessingpmj@seoultech.ac.kr>
2022-07-12 15:53:54 +0900

    [박민준] 2주차 과제_1546

---
+++ ACDCBoostCourse/ACDCBoostCourse/Section 1_준비/Week 2/2주차 과제_1546.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	double num_array[10] = {0};
+	int N;
+	double sum=0;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int grade;
+		cin >> grade;
+		num_array[i] = grade;
+	}
+	sort(num_array, num_array + N);
+	for (int i = 0; i < N; i++) {
+		sum += num_array[i] / num_array[N - 1] * 100;
+
+	}
+	cout << sum / N;
+	return 0;
+}
