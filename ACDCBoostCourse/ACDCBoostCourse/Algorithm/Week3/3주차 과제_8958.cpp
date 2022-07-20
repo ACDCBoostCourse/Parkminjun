44145a903f9e45247d0eff8bbb5b7732fe01f67b
박민준 <blessingpmj@seoultech.ac.kr>
2022-07-20 16:53:11 +0900

    [박민준] 3주차 과제_8958

---
+++ ACDCBoostCourse/ACDCBoostCourse/Algorithm/Week3/3주차 과제_8958.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	char a[80];
+	int N,sum=0;
+	
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> a;
+		int score = 1;
+		sum = 0;
+		for (int j = 0; j < strlen(a); j++) {
+			
+			
+			if (a[j] == 'O') {
+				sum += score;
+				score++;
+			}
+			else {
+				score = 1;
+			}
+		}
+		
+		cout << sum << endl;
+		
+	}
+	
+	return 0;
+}
