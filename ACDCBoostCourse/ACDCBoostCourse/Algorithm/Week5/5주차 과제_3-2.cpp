28e9e4196672b6de1167e3f6cd1b26de1692c453
박민준 <blessingpmj@seoultech.ac.kr>
2022-08-02 22:11:09 +0900

    [박민준]5주차 과제_3-2

---
+++ ACDCBoostCourse/ACDCBoostCourse/Algorithm/Week5/5주차 과제_3-2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string.h>
+using namespace std;
+
+void swap_array(int* arr, int i, int j) {
+    int temp = 0;
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+int* sorted(int* arr, bool isAscending) {
+    int* tempArr = (int*)malloc(sizeof(int) * 4);
+
+    memcpy(tempArr, arr, sizeof(int) * 4);
+
+    if (isAscending) {
+        for (int i = 0; i < 4; i++) {
+            int min = tempArr[i];
+            int location = i;
+            for (int j = i + 1; j < 4; j++) {
+                if (min > tempArr[j]) {
+                    min = tempArr[j];
+                    location = j;
+                }
+            }
+            swap_array(tempArr, i, location);
+        }
+    }
+
+    return tempArr;
+}
+
+int main() {
+    int items[4] = { 1, 5, 3, 2 };
+    int* newItems;
+    newItems = sorted(items, true);
+
+    for (int item : items) {
+        cout << item << ' '; // 1 5 3 2
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < 4; i++) {
+        cout << newItems[i] << ' ';
+    }
+}
