ada49771a8a9c7eba48ec27e9330346615ab2f35
박민준 <blessingpmj@seoultech.ac.kr>
2022-08-09 18:09:22 +0900

    [박민준] 6주차 과제_1_백준4949

---
+++ ACDCBoostCourse/ACDCBoostCourse/Algorithm/Week6/6주차 과제_1_백준4949.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+//백준은 틀리는데 예시 입출력은 잘나와서 어느부분을 고쳐야할지..? 모르겠습니다ㅠ
+
+int main() { 
+	string s;
+	
+	while (1) {
+		getline(cin, s);
+		bool tof = true;
+		if (s[0] == '.') {
+			break;
+		}
+		stack <char> stack1;
+		for (int i = 0; i < s.length(); i++) {
+			if (s[i] == '[' || s[i] == '(') {
+				stack1.push(s[i]);
+			}
+			else if (s[i] == ']') {
+				if (stack1.empty()) {
+					tof = false;
+					break;
+				}
+				else if (stack1.top() == '[') {
+					stack1.pop();
+				}
+			}
+			else if (s[i] == ')') {
+				if (stack1.empty()) {
+					tof = false;
+					break;
+				}
+				else if (stack1.top() == '(') {
+					stack1.pop();
+				}
+			}
+
+		}
+		if (tof == true && stack1.empty()) { cout << "yes" << endl; }
+		else { cout << "no" << endl; }
+	}
+}
